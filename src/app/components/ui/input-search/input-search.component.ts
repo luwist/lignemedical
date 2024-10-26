144691e0ad39839b19faad4ac3879ee87c8c5b4f
Javier Vilchez <vilchezjavierf@gmail.com>
2024-10-26 15:09:32 -0300

    create input search

---
+++ src/app/components/ui/input-search/input-search.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-search',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './input-search.component.html',
+  styleUrl: './input-search.component.scss',
+})
+export class InputSearchComponent {
+  isFocused: boolean = false;
+
+  onFocus(): void {
+    this.isFocused = true;
+  }
+
+  onBlur(): void {
+    this.isFocused = false;
+  }
+
+  onMouseover(): void {
+    this.isFocused = true;
+  }
+
+  onMouseleave(): void {
+    this.isFocused = false;
+  }
+}
